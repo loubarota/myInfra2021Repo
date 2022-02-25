fd393a2c2f81f1894b1bec5566d748b0cc6f074d
loubarota <97638188+loubarota@users.noreply.github.com>
2022-02-25 16:59:05 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket"
+    bucket = "loub-azure-aws-tf-bucket-1"
     key = "main"
     region = "us-east-2"
-    dynamodb_table = "my-dynamodb-table"
+    dynamodb_table = "azure-aws-tf-table"
   }
 }
